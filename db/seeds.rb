f0ef31c3f542bc52449ab5024da6f8ec6ed93035
Florian Theis <florian.theis@ib-theis.de>
2023-08-22 11:48:08 +0200

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'deleting all products from DB...'
+Product.destroy_all
+
+puts 'deleting all pruchases from DB...'
+Purchase.destroy_all
+
+puts 'Creating 10 new products...'
+
+10.times do
+  product = Product.new(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price,
+    description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false),
+    category: 'accessories',
+    user_id: 1
+  )
+  product.photos.attach(
+    io: File.open(Rails.root.join('app/assets/images/bugaboo.jpg')),
+    filename: 'kinderwagen',
+    content_type: 'image/jpg'
+  )
+  product.photos.attach(
+    io: File.open(Rails.root.join('app/assets/images/der-kinderwagen.jpg')),
+    filename: 'kinderwagen 2',
+    content_type: 'image/jpg'
+  )
+end
+
+puts 'finished'
