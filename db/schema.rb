9dcea7a93a73b98abfd7cdf4373221085bdbf741
Isabel Dockemeyer <isabel.dockemeyer@web.de>
2023-08-25 14:42:37 +0200

    Finsihed Basket

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_24_101028) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_25_102243) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_24_101028) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.text "description"
+    t.boolean "purchased", default: false
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
